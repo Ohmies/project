7b2959a654a7268cc660082038393d9e81801619
OHMies <Ohmperapatsingpan.15@gmail.com>
2024-09-19 19:06:43 +0700

    Latest1

--- lib/actions/auth.actions.ts
+++ lib/actions/auth.actions.ts
@@ -1,7 +1,16 @@
 "use server";
 
+import { LoginFormSchema } from "../validations/auth.validations";
+
 export async function loginAction(username: string, pin: string) {
     try {
-
-    } chat
+    // Validate input data
+    const validData = LoginFormSchema.safeParse({ username, pin });
+    if (!validData.success) {
+      return { success: false, error: "Invalid data." };
+    }
+    } catch (error) {
+        console.error("Error updating cash details:", error);
+        return false; 
+      }
 }
